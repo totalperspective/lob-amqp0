13d32b7101a9bad1156b5153e727f4ffe28e5e99
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-01-21 11:05:27 +0000

    New project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lob-ampq0
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lob_ampq0/core_test.clj
@@ -0,0 +1,7 @@
+(ns lob-ampq0.core-test
+  (:require [clojure.test :refer :all]
+            [lob-ampq0.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
