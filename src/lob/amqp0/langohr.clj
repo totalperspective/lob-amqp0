48676f9b0d444f10d527f9cbf5e91925cbdf985e
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-01-21 16:58:54 +0000

    Subscriptions defined

--- src/lob/amqp0/langohr.clj
+++ src/lob/amqp0/langohr.clj
@@ -4,6 +4,7 @@
             [langohr.queue :as lq]
             [langohr.consumers :as lc]
             [langohr.basic :as lb]
+            [langohr.exchange  :as le]
             [lob.link :as link]
             [lob.message :as msg]))
 
@@ -33,6 +34,29 @@
       (assoc :conn nil)
       (assoc :chan nil)))
 
+(defrecord Subscription [chan tag]
+  link/Subscription
+  (-unsubscribe! [_]
+    (lb/cancel chan tag)))
+
+(defrecord Publication [link chan key sub-id pub-id]
+  link/Publication
+  (-subscribe! [_ buffer-size callback]
+    (let [opts {}
+          tag (lc/subscribe chan
+                            sub-id
+                            (fn [ch meta ^bytes payload])
+                            opts)]
+      (lb/qos chan buffer-size) ;; TODO: This is not right
+      (->Subcription chan tag)))
+  link/Sender
+  (-send! [_ msg]
+    (if (link/closed? link)
+      false
+      (let [content (msg/content msg) ;; FIXME: This is not right at all
+            opts {:content-type (msg/content-type)}]
+        (lb/publish chan pub-id (str key) content opts)))))
+
 (defrecord Link [config state]
   link/Link
   (-open! [_]
@@ -43,7 +67,25 @@
     true)
   (-closed? [_]
     (nil? (:chan @state)))
-  (-publication [_ id]))
+  (-publication [this id opts]
+    (when-not (link/closed? this)
+      (let [{:keys [multicast persistent durable key]} opts
+            {:keys [chan]} @state
+            config {:durable durable
+                    :exclusive false
+                    :auto-delete (not persistent)}
+            ex-type (if (true? multicast) "fanout" multicast)]
+        (if multicast
+          (do
+            (le/declare chan id ex-type config)
+            (let [q-config {:exclusive false
+                            :auto-delete true}
+                  queue (lq/declare-server-named chan q-config)]
+              (lq/bind chan queue id)
+              (->Publication this chan key queue id)))
+          (do
+            (lq/declare chan id config)
+            (->Publication this chan key id id)))))))
 
 (defn create-link [config]
   (->Link config (atom {})))
