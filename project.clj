5d07b4a625d5e220d70a1aacb5d82e8a8efc3f70
Neel Upadhyaya <neel.upadhyaya@eckoh.com>
2016-02-01 11:09:47 +0000

    Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject totalperspective/lob-ampq0 "0.1.0"
+(defproject totalperspective/lob-ampq0 "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
